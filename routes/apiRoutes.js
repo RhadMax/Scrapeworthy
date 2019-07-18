c68014a07fdbcc6fb5e21078d45a8b6077a2ba6c
Max Patten <amanthus2535@gmail.com>
2019-07-17 23:40:09 -0700

    set up scrape route successfully, pulling in title and link from source site and adding to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,5 +3,27 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 module.exports = app => {
+    app.get("/scrape", function (req, res) {
+        axios.get("https://www.pcgamer.com/news/").then(function (response) {
+            var $ = cheerio.load(response.data);
+            $("div.listingResult").each(function (i, element) {
+                var result = {};
 
+                result.title = $(element)
+                    .find("h3")
+                    .text();
+                result.link = $(element)
+                    .find("a.article-link")
+                    .attr("href");
+
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                    })
+                    .catch(function (err) {
+                        console.log("Something went wrong retrieving articles with scraper.");
+                    });
+            });
+            res.status(200).send("Scrape Complete");
+        });
+    });
 };
